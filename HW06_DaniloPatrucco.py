deb2a92efe3936812fad140ff7a6344b260784ce
Zantezu <danilo.patrucco@gmail.com>
2019-10-03 18:29:30 -0400

    Task: Problem 1 completed

--- HW06_DaniloPatrucco.py
+++ HW06_DaniloPatrucco.py
@@ -3,3 +3,23 @@
 # HW 06, October 2, 2019
 
 # Exercise 1
+
+def twoWords(lenght, firstLetter):
+    oneWord = '0'
+    wordLenght = 0
+    lst = []
+    while lenght != wordLenght:
+        wordLenght = str(input('Enter a ' + str(lenght) + ' -letter word please: '))
+        if lenght == len(wordLenght):
+            lst.append(wordLenght)
+            break
+    while firstLetter != oneWord[0]:
+        oneWord = str(input('Enter a word beginning with ' + str(firstLetter) + ' please: '))
+        if oneWord[0] == firstLetter:
+            lst.append(oneWord)
+            break
+    return lst
+
+
+print("\r\nProblem 1: \r\n")
+print(twoWords(4, 'B'))
