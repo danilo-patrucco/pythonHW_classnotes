d15255575150cc60f6421961c631f51d69ce39a3
zantezu <danilo.patrucco@gmail.com>
2019-10-09 20:33:50 -0400

    fix : added some blocks of code

--- exam.py
+++ exam.py
@@ -1,3 +1,4 @@
+'''
 import turtle
 
 
@@ -20,3 +21,62 @@ t = turtle.Turtle()
 staircase()
 
 
+def first_last(string_list):
+    returnList = []
+    for string in string_list:
+        if string != '' and string[0] == string[-1]:
+            returnList.append(string)
+    return returnList
+
+
+
+
+def vowel_tracker(words):
+    init_vowel = 0
+    ending_vowel = 0
+    vowel_lst = 'aeiouAEIOU'
+    for i in words:
+        if i[0] in vowel_lst:
+            init_vowel += 1
+        if i[-1] in vowel_lst:
+            ending_vowel += 1
+    return [init_vowel, ending_vowel]
+
+
+
+
+
+# review this
+def number_luck(lucky, unlucky):
+    num = int(input('give me a number between 2 and 12'))
+    int_loop=[]
+    int_loop.append(lucky)
+    int_loop.append(unlucky)
+    for i in int_loop:
+        if i in lucky:
+            if i == num:
+                print('number is lucky')
+        elif i in unlucky:
+            if i == num:
+                print('number is unlucky')
+        else:
+            print('your number is boring')
+    return
+
+l = [3,4,5]
+u = [6,7,8]
+
+'''
+
+def month_info(medium_lenght):
+    month = str(input('What month is it?'))
+    days = int(input('How many days in a month'))
+    if days < medium_lenght:
+        print('month is short')
+    elif days == medium_lenght:
+        print('month is medium')
+    else :
+        print('month is long')
+    return month
+
+print(month_info(30))
