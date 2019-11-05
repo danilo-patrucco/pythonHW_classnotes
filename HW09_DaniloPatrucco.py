c3bd033c8ee58e66c84e229dae39e5486248bdf9
zantezu <danilo.patrucco@gmail.com>
2019-11-04 22:10:18 -0500

    Task: prepped hw 09

---
+++ HW09_DaniloPatrucco.py
@@ -0,0 +1,13 @@
+# Danilo Patrucco
+# CS 100 2019F Section 103
+# HW 09, November 04, 2019
+
+
+
+print("\r\nProblem 1: \r\n")
+
+
+print("\r\nProblem 2: \r\n")
+
+
+print("\r\nProblem 3: \r\n")
--- class exercises.py
+++ class exercises.py
@@ -149,3 +149,4 @@ ignore(table)
 """
 
 
+
