8eb425312adc26c93d58059325bcbd80e3ba8d7e
zantezu <danilo.patrucco@gmail.com>
2019-11-20 18:32:11 -0500

    Fix : added missing part to HW 10 (next time check for page breaks !)

--- dog.py
+++ dog.py
@@ -1,13 +1,30 @@
 class Dog:
     ''' Danilo Patrucco - CS100-103 - 11/17/2019 - HW 10'''
 
+    species = 'canis lupus familiaris'
+
     def __init__(self,name, breed):
         self.name = name
         self.breed = breed
         self.tricks = []
 
-    def teach(self, tricks):
-        self.tricks.append(tricks)
-        print('Sugar knows ' + self.tricks[-1])
+    def teach(self, trick):
+        if trick not in self.tricks:
+            self.tricks.append(trick)
+            print(self.name + ' ' + trick)
+        else:
+            print(self.name, 'already know', trick)
+
+# Problem 4
+
+    def knows (self, trick):
+        if trick in self.tricks:
+            print('Yes,', self.namem, self.species, trick)
+            return True
+        else:
+            print('No,', self.namem, self.species, trick)
+            return False
+
+
 
 
